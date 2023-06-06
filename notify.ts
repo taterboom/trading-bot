ca2f853d0369104c37fe90da2b19bbbcf4592dde
xueyong <1085523029@qq.com>
2023-06-06 15:29:06 +0800

    ⚡️ use dialog instead of notification

--- notify.ts
+++ notify.ts
@@ -28,5 +28,8 @@ extra: ${strategy.extra || ""}`
 }
 
 export function displayNotificationOnMac(title: string, body: string) {
-  exec(`osascript -e 'display notification "${body}" with title "${title}"'`)
+  exec(
+    `osascript -e 'display alert "${title}" message "${body}" buttons {"OK", "Cancel"} default button "OK" cancel button "Cancel"'`
+  )
+  // exec(`osascript -e 'display notification "${body}" with title "${title}"' giving up after 35`)
 }
